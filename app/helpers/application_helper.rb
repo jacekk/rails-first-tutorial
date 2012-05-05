db0795114b51ec11de900d40e38226616b765a61
jacekk <info@jacek-k.net>
2012-05-05 23:17:30 +0200

    Adde a title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+	
+	# Returns a title on a per-page title
+	def title
+		base_title = "RoR Tut"
+		if @title.nil?
+			base_title
+		else 
+			"#{@title} | #{base_title}"
+		end
+	end
 end
